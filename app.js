4fea5f5fbbd23f2fc60b8fa863ef3ff1a078cc7c
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-08-21 23:59:28 +0300

    update migrations

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 require('dotenv').config();
-
+const knex = require('./knex.js');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,28 +17,14 @@ app.use(bodyParser.json());
 const { addList, getLists, deleteList, getList } = require('./routes/index');
 const { addTask, editPage, editTask, deleteTask } = require('./routes/tasks');
 
-var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-});
-
-con.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('Connected to database');
-});
-global.con = con;
-
 app.get('/', getLists);
 app.post('/', addList);
-app.get('/delete/:id', deleteList);
+app.delete('/delete/:id', deleteList);
 app.get('/todo/:id', getList);
 app.post('/todo/:id', addTask);
-app.post('/todo/edit/:id', editPage);
+app.put('/todo/edit/:id', editPage);
 app.get('/todo/edit/:id', editTask);
-app.get('/todo/delete/:id', deleteTask);
+app.delete('/todo/delete/:id', deleteTask);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
