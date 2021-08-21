bffbe60f177ac80fd461b9ed295df7f675b969a6
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-08-21 22:33:47 +0300

    install knex

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER_NAME,
+      password: process.env.DB_USER_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
